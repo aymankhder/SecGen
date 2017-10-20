e0608685e80fab12a8750111f42a51533a148534
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2017-10-20 03:03:02 +0100

    WiP

--- modules/generators/structured_content/hackerbot_config/ids/secgen_local/local.rb
+++ modules/generators/structured_content/hackerbot_config/ids/secgen_local/local.rb
@@ -5,6 +5,7 @@ class IDS < HackerbotConfigGenerator
 
   attr_accessor :web_server_ip
   attr_accessor :ids_server_ip
+  attr_accessor :hackerbot_server_ip
 
   def initialize
     super
@@ -18,11 +19,13 @@ class IDS < HackerbotConfigGenerator
 
     self.web_server_ip = []
     self.ids_server_ip = []
+    self.hackerbot_server_ip = []
   end
 
   def get_options_array
     super + [['--web_server_ip', GetoptLong::REQUIRED_ARGUMENT],
-             ['--ids_server_ip', GetoptLong::REQUIRED_ARGUMENT]]
+             ['--ids_server_ip', GetoptLong::REQUIRED_ARGUMENT],
+             ['--hackerbot_server_ip', GetoptLong::REQUIRED_ARGUMENT]]
   end
 
   def process_options(opt, arg)
@@ -31,6 +34,8 @@ class IDS < HackerbotConfigGenerator
       when '--web_server_ip'
         self.web_server_ip << arg;
       when '--ids_server_ip'
+        self.ids_server_ip << arg;
+      when '--hackerbot_server_ip'
         self.ids_server_ip << arg;
     end
   end
--- modules/utilities/unix/monitoring_ids/wireshark/manifests/install.pp
+++ modules/utilities/unix/monitoring_ids/wireshark/manifests/install.pp
@@ -1,5 +1,5 @@
 class wireshark::install{
-  package { ['wireshark']:
+  package { ['wireshark', 'tcpdump']:
     ensure => 'installed',
   }
 }
