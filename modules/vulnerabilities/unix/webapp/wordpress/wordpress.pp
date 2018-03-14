917fc96074a8c419aa6ea8c46cf7ba5fe6d41a38
thomashaw <thomashaw@gmail.com>
2018-03-14 15:58:45 +0000

    wordpress working - need to add account configuration automation, this will require checking wp versions + changing the post request as later versions have different fields.

--- modules/vulnerabilities/unix/webapp/wordpress/wordpress.pp
+++ modules/vulnerabilities/unix/webapp/wordpress/wordpress.pp
@@ -1,16 +1,21 @@
-class { 'apache':
-  mpm_module => 'prefork',
-}
+$secgen_parameters = ::secgen_functions::get_parameters($::base64_inputs_file)
+$version = $secgen_parameters['version'][0]
 
-class { 'apache::mod::php': }
 class { 'mysql::server': }
 class { 'mysql::bindings': php_enable => true, }
 
+class { '::apache':
+  default_vhost => false,
+  overwrite_ports => false,
+  default_mods => ['rewrite', 'php'],
+}
+
 apache::vhost { 'wordpress':
-  docroot => '/opt/wordpress',
+  docroot => '/var/www/wordpress',
   port    => '80',
 }
 
 class { 'wordpress':
-  # version => '3.4',
+  install_dir => '/var/www/wordpress',
+  version => $version,
 }
