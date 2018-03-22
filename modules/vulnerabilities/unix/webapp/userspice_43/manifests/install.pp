56ac9e2e7dce49a863f6f9f7a269e24bef6529c9
thomashaw <thomashaw@gmail.com>
2018-03-22 19:09:22 +0000

    WiP userspice cron + test_scenario.xml

--- modules/vulnerabilities/unix/webapp/userspice_43/manifests/install.pp
+++ modules/vulnerabilities/unix/webapp/userspice_43/manifests/install.pp
@@ -34,9 +34,4 @@ class userspice_43::install {
     content => template('userspice_43/userspice.sh.erb'),
     require => File[$docroot],
   }
-  ~>
-  cron { 'run userspice config script':
-    command => '/bin/bash /userspice.sh',
-    minute => [0, 5,10,15,20,25,30,35,40,45,50,55]
-  }
 }
