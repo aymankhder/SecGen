341cd0bdf6df9da71bf882fd6625cec7010f69a5
ts <thomashaw@localhost.localdomain>
2019-02-05 17:28:15 +0000

    vulnerabilities/unix/local/dirtycow

--- modules/utilities/unix/update/apt_upgrade/manifests/apt.pp
+++ modules/utilities/unix/update/apt_upgrade/manifests/apt.pp
@@ -1,17 +1,28 @@
 class apt_upgrade::apt {
-  case $operatingsystem {
-    'Debian': {
-      exec { 'update':
-        command => "/usr/bin/apt-get upgrade",
-        tries => 5,
-        try_sleep => 30,
+
+  notice("Running apt-upgrade module...")
+
+  if defined('dirtycow::config') {
+    notice("vulnerabilities/unix/local/dirtycow included - skipping apt-get upgrade...")
+  } else {
+    case $operatingsystem {
+      'Debian': {
+        exec { 'update':
+          command   => "/usr/bin/apt-get -y upgrade",
+          tries     => 5,
+          try_sleep => 30,
+          timeout => 0,
+          logoutput   => true,
+        }
       }
-    }
-    'Ubuntu': {
-      exec { 'update':
-        command => "/usr/bin/apt-get upgrade",
-        tries => 5,
-        try_sleep => 30,
+      'Ubuntu': {
+        exec { 'update':
+          command   => "/usr/bin/apt-get -y upgrade",
+          tries     => 5,
+          try_sleep => 30,
+          timeout => 0,
+          logoutput   => true,
+        }
       }
     }
   }
