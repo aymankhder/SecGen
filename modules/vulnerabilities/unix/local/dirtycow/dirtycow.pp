3e4123642c0fb0dc2acd65fe98b216ac9a5562b1
thomashaw <thomashaw@gmail.com>
2019-02-07 12:57:18 +0000

    reverting dirtycow for now, need to exclude the puppet package from apt-get upgrade before inclusion

--- modules/vulnerabilities/unix/local/dirtycow/dirtycow.pp
+++
@@ -1,1 +0,0 @@
-include dirtycow::config
--- modules/vulnerabilities/unix/local/dirtycow/manifests/config.pp
+++
@@ -1,3 +0,0 @@
-class dirtycow::config {
-  notice("dirtycow::config: Do nothing, the apt upgrade just checks if we're defined and blocks apt-get upgrade if so.")
-}
