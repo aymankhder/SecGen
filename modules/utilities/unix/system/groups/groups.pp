16b5d91951c1b59879a6c05267636a7d78e3b9e6
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2019-02-07 10:49:23 +0000

    lab updates

---
+++ modules/utilities/unix/system/groups/groups.pp
@@ -0,0 +1,1 @@
+require groups::init
---
+++ modules/utilities/unix/system/groups/manifests/init.pp
@@ -0,0 +1,13 @@
+class groups::init {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+
+  $groups = $secgen_parameters['accounts']
+  if $groups {
+    $groups.each |$group| {
+
+      group { "$group":
+        ensure => 'present',
+      }
+    }
+  }
+}
