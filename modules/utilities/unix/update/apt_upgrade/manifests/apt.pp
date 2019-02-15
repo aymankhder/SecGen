9293aba51ffb0aebc5a37280c19bbf9bd1ca9d28
ts <thomashaw@localhost.localdomain>
2019-02-15 17:25:26 +0000

    utilities/apt_upgrade: hold puppet-agent to prevent breaking on oVirt

--- modules/utilities/unix/update/apt_upgrade/manifests/apt.pp
+++ modules/utilities/unix/update/apt_upgrade/manifests/apt.pp
@@ -7,12 +7,17 @@ class apt_upgrade::apt {
   } else {
     case $operatingsystem {
       'Debian': {
+        # can't upgrade puppet agent mid-provision or it breaks on oVirt.
+        exec { 'hold puppet-agent':
+          command => '/usr/bin/apt hold puppet-agent'
+        }
         exec { 'update':
           command   => "/usr/bin/apt-get -y upgrade",
           tries     => 5,
           try_sleep => 30,
           timeout => 0,
           logoutput   => true,
+          require => Exec['hold puppet agent'],
         }
       }
       'Ubuntu': {
