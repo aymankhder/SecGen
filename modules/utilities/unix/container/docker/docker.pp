b9cc3ad313bae37ae4ccd7e6eefc55e7d3e2b919
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2019-03-12 12:47:10 +0000

    lab updates

--- modules/utilities/unix/container/docker/docker.pp
+++ modules/utilities/unix/container/docker/docker.pp
@@ -1,1 +1,10 @@
+# install
 include 'docker'
+
+# download (pull) a set of images
+$secgen_params = secgen_functions::get_parameters($::base64_inputs_file)
+$images = $secgen_params['images']
+
+$images.each |$image| {
+  docker::image { "$image": }
+}
