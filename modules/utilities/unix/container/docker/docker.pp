52c51484d1b710c4d26ff6c9cf55274470144739
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2019-03-15 16:44:40 +0000

    lab updates

--- modules/utilities/unix/container/docker/docker.pp
+++ modules/utilities/unix/container/docker/docker.pp
@@ -1,10 +1,15 @@
-# install
-include 'docker'
-
-# download (pull) a set of images
 $secgen_params = secgen_functions::get_parameters($::base64_inputs_file)
 $images = $secgen_params['images']
 
+# install
+include 'docker'
+
+# TODO: configure proxy via secgen argument?
+#class { 'docker':
+#  proxy => "http://172.22.0.51:3128",
+#}
+
+# download (pull) a set of images
 $images.each |$image| {
   docker::image { "$image": }
 }
