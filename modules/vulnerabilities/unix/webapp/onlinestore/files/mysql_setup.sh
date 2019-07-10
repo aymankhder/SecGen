c80a7a155898b531a0fab4be0f6d3b1da2b88745
ts <thomashaw@localhost.localdomain>
2019-07-10 14:49:37 +0100

    vulnerabilities/unix/webapp/onlinestore: token table flag fix

--- modules/vulnerabilities/unix/webapp/onlinestore/files/mysql_setup.sh
+++ modules/vulnerabilities/unix/webapp/onlinestore/files/mysql_setup.sh
@@ -2,11 +2,10 @@
 
 USERNAME=${1}
 PASSWORD=${2}
-token=${3}
 
 echo "CREATE USER '${USERNAME}'@'localhost' IDENTIFIED BY '${PASSWORD}';"| mysql --force
 echo "GRANT ALL PRIVILEGES ON * . * TO '${USERNAME}'@'localhost';"| mysql --force
 echo "CREATE DATABASE csecvm;"| mysql --user=${USERNAME} --password=${PASSWORD} --force
 mysql --force --user=${USERNAME} --password=${PASSWORD} csecvm < ./csecvm.sql
 
-echo "USE csecvm; INSERT INTO token VALUES ('${token}');" |  mysql --force --user=${USERNAME} --password=${PASSWORD}
+echo "USE csecvm;" |  mysql --force --user=${USERNAME} --password=${PASSWORD}
