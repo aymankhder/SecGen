77dcd2ff262a539cc09be33adf6454cbe656b87b
ts <thomashaw@localhost.localdomain>
2019-10-29 11:36:55 +0000

    utilities/unix_update: https support for apt repos in debian

--- modules/utilities/unix/update/unix_update/manifests/unix.pp
+++ modules/utilities/unix/update/unix_update/manifests/unix.pp
@@ -2,7 +2,7 @@ class unix_update::unix{
   case $operatingsystem {
     'Debian': {
       exec { 'update':
-        command => "/usr/bin/apt-get update --fix-missing",
+        command => "/usr/bin/apt-get install apt-transport-https ca-certificates --fix-missing -y && /usr/bin/apt-get update --fix-missing",
         tries => 5,
         try_sleep => 30,
       }
