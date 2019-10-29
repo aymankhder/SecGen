2c9bd2f34de2ca742916beb2e176e835f7bd74a7
ts <thomashaw@localhost.localdomain>
2019-10-29 11:37:51 +0000

    WIP: ELK + Wazuh installing correctly

--- modules/utilities/unix/logging/wazuh/wazuh.pp
+++ modules/utilities/unix/logging/wazuh/wazuh.pp
@@ -1,14 +1,15 @@
 $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
 $component = $secgen_parameters['component'][0]
 if ($component == 'server') {
-  class { '::wazuh::server':
-    smtp_server   => 'localhost',
+  class { '::wazuh::manager':
+    ossec_smtp_server   => 'localhost',
     ossec_emailto => ['user@mycompany.com'],
     agent_auth_password => '6663484170b2c69451e01ba11f319533', #todo: obviously fix this - must be 32char
   }
 } elsif ($component == 'client') {
-  class { "::wazuh::client":
-    ossec_server_ip => "192.168.209.166",
+  class { "::wazuh::agent":
+    wazuh_register_endpoint => "192.168.209.166",
+    wazuh_reporting_endpoint => "192.168.209.166",
     agent_name => 'test_name',
   }
 }
