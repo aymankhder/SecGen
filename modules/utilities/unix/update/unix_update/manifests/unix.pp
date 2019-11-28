671a929b6149b34dff0f7e21f0188a570a9015b0
ts <thomashaw@localhost.localdomain>
2019-11-28 15:25:12 +0000

    Changed ordering of installing apt https + ca-certificates

--- modules/utilities/unix/update/unix_update/manifests/unix.pp
+++ modules/utilities/unix/update/unix_update/manifests/unix.pp
@@ -2,7 +2,7 @@ class unix_update::unix{
   case $operatingsystem {
     'Debian': {
       exec { 'update':
-        command => "/usr/bin/apt-get install apt-transport-https ca-certificates --fix-missing -y && /usr/bin/apt-get update --fix-missing",
+        command => "/usr/bin/apt-get update --fix-missing && /usr/bin/apt-get install apt-transport-https ca-certificates --fix-missing -y",
         tries => 5,
         try_sleep => 30,
       }
