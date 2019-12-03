4237dce790632d69f49e3119d23d135d874bb198
ts <thomashaw@localhost.localdomain>
2019-12-03 13:31:57 +0000

    WIP: adding http.host to logstash config

--- modules/utilities/unix/logging/logstash/logstash.pp
+++ modules/utilities/unix/logging/logstash/logstash.pp
@@ -3,8 +3,11 @@ $logstash_port = 0 + $secgen_parameters['logstash_port'][0]
 $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
 $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
 
-include logstash
-
+class { 'logstash':
+  settings => {
+    'http.host' => $elasticsearch_ip,
+  }
+}
 logstash::configfile { 'my_ls_config':
   content => template('logstash/configfile-template.erb'),
 }
