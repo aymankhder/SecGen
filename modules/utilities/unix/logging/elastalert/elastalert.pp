f3af96f123087f8b52278ec149533ec8cd4e688b
ts <thomashaw@localhost.localdomain>
2019-12-05 17:17:02 +0000

    elastalert stuff

---
+++ modules/utilities/unix/logging/elastalert/elastalert.pp
@@ -0,0 +1,1 @@
+include elastalert::install
---
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -0,0 +1,9 @@
+class elastalert::configure {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
+  $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
+
+  ensure_packages('pip3')
+  ensure_packages(['elastalert', 'setuptools>=11.3'], { 'package' => 'pip3', 'require' => 'pip3' })
+
+}
