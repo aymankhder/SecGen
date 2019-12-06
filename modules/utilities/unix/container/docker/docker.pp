e3660af2f8e65f52ea6399d2606620111b729166
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2019-12-06 17:02:44 +0000

    docker behind proxy to be configured in base

--- modules/utilities/unix/container/docker/docker.pp
+++ modules/utilities/unix/container/docker/docker.pp
@@ -9,29 +9,44 @@ include 'docker'
 #  proxy => "http://172.22.0.51:3128",
 #}
 
+
+# remove proxy config (it's in the template, and this overrides)
+# file_line { 'remove_docker_proxy1':
+#   ensure            => absent,
+#   path              => '/etc/default/docker',
+#   line              => 'HTTP_PROXY=""',
+# } ->
+# file_line { 'remove_docker_proxy2':
+#   ensure            => absent,
+#   path              => '/etc/default/docker',
+#   line              => 'http_proxy=""',
+# } ->
+# file_line { 'remove_docker_proxy3':
+#   ensure            => absent,
+#   path              => '/etc/default/docker',
+#   line              => 'https_proxy=""',
+# } ->
+# file_line { 'remove_docker_proxy4':
+#   ensure            => absent,
+#   path              => '/etc/default/docker',
+#   line              => 'HTTPS_PROXY=""',
+# } ->
+# file_line { 'remove_docker_proxy_settings':
+#   ensure => absent,
+#   path => '/etc/default/docker',
+#   match => '^HTTP*_PROXY=*',
+#   match_for_absence => true,
+#   multiple => true,
+#   notify  => Service['docker'],  # reload docker once changed
+# }
+#
+# # remove proxy config (it's in the template, and this overrides)
+# exec { 'docker_remove_config_and_restart':
+#   command  => "mv /etc/default/docker /etc/default/docker.mv; systemctl daemon-reload; systemctl restart docker",
+#   provider => shell,
+# }
+
 # download (pull) a set of images
 $images.each |$image| {
   docker::image { "$image": }
-}
-
-# remove proxy config (it's in the template, and this overrides)
-file_line { 'remove_docker_proxy1':
-  ensure            => absent,
-  path              => '/etc/default/docker',
-  line              => 'HTTP_PROXY=""',
-}
-file_line { 'remove_docker_proxy2':
-  ensure            => absent,
-  path              => '/etc/default/docker',
-  line              => 'http_proxy=""',
-}
-file_line { 'remove_docker_proxy3':
-  ensure            => absent,
-  path              => '/etc/default/docker',
-  line              => 'https_proxy=""',
-}
-file_line { 'remove_docker_proxy4':
-  ensure            => absent,
-  path              => '/etc/default/docker',
-  line              => 'HTTPS_PROXY=""',
 }
