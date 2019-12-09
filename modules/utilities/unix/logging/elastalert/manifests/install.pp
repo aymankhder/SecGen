626028463930acb97168deac7bf9a93cfe1ed026
ts <thomashaw@localhost.localdomain>
2019-12-09 10:39:13 +0000

    renamed class

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -1,4 +1,4 @@
-class elastalert::configure {
+class elastalert::install {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
   $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
   $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
