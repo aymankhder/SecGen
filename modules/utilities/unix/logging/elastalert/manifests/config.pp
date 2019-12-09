6ff57ec092753e229c35cc0868e1c667b2ce0bfa
ts <thomashaw@localhost.localdomain>
2019-12-09 13:11:13 +0000

    updated yml to yaml, copy rules directory

--- modules/utilities/unix/logging/elastalert/manifests/config.pp
+++ modules/utilities/unix/logging/elastalert/manifests/config.pp
@@ -2,16 +2,23 @@ class elastalert::config {
   $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
   $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
   $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
+  $elastalert_dir = '/opt/elastalert/'
+  $rules_dir = '/opt/elastalert/rules/'
 
-
-  file { ['/opt/elastalert/', '/opt/elastalert/rules/']:
+  file { $elastalert_dir:
     ensure => directory,
   }
 
-  file { '/opt/elastalert/config.yml':
+  file { '/opt/elastalert/config.yaml':
     ensure => file,
-    content => template('elastalert/config.yml.erb'),
-    require => File['/opt/elastalert/'],
+    content => template('elastalert/config.yaml.erb'),
+    require => File[$elastalert_dir],
   }
 
+  file { $rules_dir:
+    ensure => directory,
+    recurse => true,
+    source => 'puppet:///modules/elastalert/rules/',
+    require => File[$elastalert_dir],
+  }
 }
