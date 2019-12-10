a957b2a8a5e62fb6bfde57e6cb5085a33301a30c
ts <thomashaw@localhost.localdomain>
2019-12-10 18:21:46 +0000

    fixed example-rule.yaml formatting + added .diff file with fix

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -3,7 +3,18 @@ class elastalert::install {
   $elasticsearch_ip = $secgen_parameters['elasticsearch_ip'][0]
   $elasticsearch_port = 0 + $secgen_parameters['elasticsearch_port'][0]
 
+  Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
+
   ensure_packages('python3-pip')
   ensure_packages(['elastalert', 'setuptools>=11.3', 'PyYAML>=5.1', 'elasticsearch==6.3.1'], { provider => 'pip3', require => [Package['python3-pip']] })
+
+  file { '/opt/elastalert/elastalert.diff':
+    ensure => file,
+    source => 'puppet:///modules/elastalert/elastalert.diff',
+  }
+
+  # exec { 'apply diff patch':
+    # command => 'git '
+  # }
 
 }
