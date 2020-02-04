1d8a334b5e415235e9d4289d16469d7e211c61c5
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2020-02-04 16:10:49 +0000

    stretch fix for root xfce login

--- modules/vulnerabilities/unix/desktop/xfce_lightdm_root_login/manifests/configure.pp
+++ modules/vulnerabilities/unix/desktop/xfce_lightdm_root_login/manifests/configure.pp
@@ -1,12 +1,17 @@
 class xfce_lightdm_root_login::configure {
   case $operatingsystemrelease {
     /^9.*/: { # do 9.x stretch stuff
-      exec { 'lightdm-autologin-remove-pam':
-        command => "/bin/sed -i \'/auth      required pam_succeed_if.so user != root quiet_success/s/^/#/g\' /etc/pam.d/lightdm-autologin"
-      }
       file { '/etc/lightdm/lightdm.conf':
         ensure => present,
         source => 'puppet:///modules/xfce_lightdm_root_login/stretch_lightdm.conf',
+      }
+      file_line { 'lightdm-autologin-remove-pam':
+        ensure  => 'present',
+        path    => '/etc/pam.d/lightdm-autologin',
+        line    => 'auth      required pam_succeed_if.so user != anything quiet_success',
+        match   => 'auth      required pam_succeed_if.so user != root quiet_success',
+        replace => true,
+        match_for_absence => 'false',
       }
     }
     /^7.*/: { # do 7.x wheezy stuff
