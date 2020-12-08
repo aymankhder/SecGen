4fd9ddee7d7a972933bb5a51d261e817586102ae
thomashaw <thomashaw@gmail.com>
2020-12-08 16:08:10 +0000

    wip

--- modules/services/unix/logging/elk_upgrade/elasticsearch_7/manifests/service.pp
+++ modules/services/unix/logging/elk_upgrade/elasticsearch_7/manifests/service.pp
@@ -2,4 +2,13 @@ class elasticsearch_7::service {
   service { 'elasticsearch':
     enable  => true,
   }
+
+  # remove startup timeout
+  file { '/etc/systemd/system/elasticsearch.service.d/':
+    ensure => directory,
+  }->
+  file { '/etc/systemd/system/elasticsearch.service.d/startup-timeout.conf':
+    ensure => present,
+    content => "[Service]\nTimeoutStartSec=180"
+  }
 }
