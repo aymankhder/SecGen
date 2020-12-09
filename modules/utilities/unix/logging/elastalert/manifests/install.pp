596392de2d388654305fedf0627bbaa48e2f3903
thomashaw <thomashaw@gmail.com>
2020-12-09 19:09:28 +0000

    wip

--- modules/utilities/unix/logging/elastalert/manifests/install.pp
+++ modules/utilities/unix/logging/elastalert/manifests/install.pp
@@ -1,7 +1,7 @@
 class elastalert::install ($elasticsearch_ip, $elasticsearch_port,$installdir = '/opt/elastalert/', $source='http://github.com/Yelp/elastalert') {
   Exec { path => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'] }
 
-  ensure_packages(['python-pip3','build-essential','libssl-dev','libffi-dev','python-dev'])
+  ensure_packages(['python3-pip','build-essential','libssl-dev','libffi-dev','python-dev'])
   ensure_packages(['PyYAML>=5.1','elastalert'], { provider => 'pip3', require => [Package['python-pip3']] })
 
   # Create directory to install into   TODO: Change this to another variable name.  Should put configs in /etc/ probably if we're installing via...
