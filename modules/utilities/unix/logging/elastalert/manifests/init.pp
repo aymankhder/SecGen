b14c9bc4ab26065ea709d0d5853444d4bcc14972
thomashaw <thomashaw@gmail.com>
2020-12-09 14:19:41 +0000

    wip

--- modules/utilities/unix/logging/elastalert/manifests/init.pp
+++ modules/utilities/unix/logging/elastalert/manifests/init.pp
@@ -2,13 +2,14 @@ class elastalert ($elasticsearch_ip, $elasticsearch_port) {
   class { 'elastalert::install':
     elasticsearch_ip => $elasticsearch_ip,
     elasticsearch_port => $elasticsearch_port,
-  } ~>
-  class {'elastalert::config':
-    elasticsearch_ip => $elasticsearch_ip,
-    elasticsearch_port => $elasticsearch_port,
-  }~>
-  class {'elastalert::service':
-    elasticsearch_ip => $elasticsearch_ip,
-    elasticsearch_port => $elasticsearch_port,
   }
+  # ~>
+  # class {'elastalert::config':
+  #   elasticsearch_ip => $elasticsearch_ip,
+  #   elasticsearch_port => $elasticsearch_port,
+  # }~>
+  # class {'elastalert::service':
+  #   elasticsearch_ip => $elasticsearch_ip,
+  #   elasticsearch_port => $elasticsearch_port,
+  # }
 }
