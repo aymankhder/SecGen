e471e6a00b52bf446ccbbbad15bff5921836331e
thomashaw <thomashaw@gmail.com>
2021-03-04 05:00:36 +0000

    ASE 5: wip

--- modules/utilities/unix/ctf/metactf/files/repository/src_sse/Ch3.7-3.9/Ch3_07_StackSmash/build.zsh
+++ modules/utilities/unix/ctf/metactf/files/repository/src_sse/Ch3.7-3.9/Ch3_07_StackSmash/build.zsh
@@ -6,7 +6,7 @@ if [[ ARGC -gt 0 ]] then
     mkdir -p obj/$USER
     AA=`echo $USER $SALT $BINNAME | openssl dgst -sha512 -binary | base64 | head -1 | tr -d /=+ | cut -c 1-3 | xxd -p | sed s/0a$/7a/`
     #gcc -o obj/$USER/$BINNAME program.c
-    gcc -no-pie -fno-pie -fno-stack-protector -Wl,--section-start=.text=0x$AA -o obj/$USER/$BINNAME program.c
+    gcc -no-pie -fno-pie -fno-stack-protector -z execstack -z norelro -Wl,--section-start=.text=0x$AA -o obj/$USER/$BINNAME program.c
   end
 else
   echo "USAGE: build.zsh <user_email(s)>"
