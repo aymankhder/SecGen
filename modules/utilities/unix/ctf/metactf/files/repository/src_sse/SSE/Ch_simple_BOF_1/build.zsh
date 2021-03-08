e055aa6f93a7cb0dce0ae441623a01b260def792
thomashaw <thomashaw@gmail.com>
2021-03-08 16:37:28 +0000

    ASE 6: wip

--- modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_simple_BOF_1/build.zsh
+++ modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_simple_BOF_1/build.zsh
@@ -4,7 +4,7 @@ if [[ ARGC -gt 0 ]] then
   BINNAME=`basename $PWD`
   foreach USER ($@)
     mkdir -p obj/$USER
-    gcc -g -m32 -z execstack -z norelro -fno-pie -no-pie -o obj/$USER/$BINNAME program.c
+    gcc -g -m32 -fno-stack-protector -z execstack -z norelro -fno-pie -no-pie -o obj/$USER/$BINNAME program.c
   end
 else
   echo "USAGE: build.zsh <user_email(s)>"
