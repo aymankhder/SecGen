47cdcdaf02a87f20371170b028d2a3aa22f81871
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2021-03-10 11:17:30 +0000

    ASE 6: wip rand

--- modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_simple_BOF_1/build.zsh
+++ modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_simple_BOF_1/build.zsh
@@ -4,7 +4,8 @@ if [[ ARGC -gt 0 ]] then
   BINNAME=`basename $PWD`
   foreach USER ($@)
     mkdir -p obj/$USER
-    cat program.c.template >! program.c
+    AA=`echo $USER $SALT $BINNAME | sha512sum | sum | cut -c 1-2 | awk '{printf "%d",$1+10}'`
+    cat program.c.template | sed s/AAAAAA/$AA/ >! program.c
     gcc -g -m32 -fno-stack-protector -z execstack -z norelro -fno-pie -no-pie -o obj/$USER/$BINNAME program.c
   end
 else
