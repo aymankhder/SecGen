c550fc6c1800fed5943c5288e7c7947737a40fe5
thomashaw <thomashaw@gmail.com>
2021-03-22 19:55:26 +0000

    MetaCTF: fix

--- modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_nx_BOF_1/build.zsh
+++ modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_nx_BOF_1/build.zsh
@@ -6,7 +6,7 @@ if [[ ARGC -gt 0 ]] then
     mkdir -p obj/$USER
 
     # No randomisation, this is a static example to demonstrate a stack-based exploit failing due to nx stack
-
+    cat program.c.template >! program.c
     gcc -g -m32 -fno-stack-protector -z norelro -fno-pie -no-pie -Wl,--section-start=.text=0x64574b7a: -mpreferred-stack-boundary=2 -o obj/$USER/$BINNAME program.c
   end
 else
