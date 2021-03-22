7797bf63ee081531bd00a7b2e6c1569863c5e73b
thomashaw <thomashaw@gmail.com>
2021-03-22 20:41:23 +0000

    MetaCTF: fix

--- modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_nx_BOF_1/build.zsh
+++ modules/utilities/unix/ctf/metactf/files/repository/src_sse/SSE/Ch_nx_BOF_1/build.zsh
@@ -7,7 +7,7 @@ if [[ ARGC -gt 0 ]] then
 
     # No randomisation, this is a static example to demonstrate a stack-based exploit failing due to nx stack
     cat program.c.template > program.c
-    gcc -g -m32 -fno-stack-protector -z norelro -fno-pie -no-pie -Wl,--section-start=.text=0x64574b7a: -mpreferred-stack-boundary=2 -o obj/$USER/$BINNAME program.c
+    gcc -g -m32 -fno-stack-protector -z norelro -fno-pie -no-pie -Wl,--section-start=.text=0x64574b7a -mpreferred-stack-boundary=2 -o obj/$USER/$BINNAME program.c
   end
 else
   echo "USAGE: build.zsh <user_email(s)>"
