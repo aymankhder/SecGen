26618d7b855866f1a2e3499a2667c30e7e7ae8f8
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2021-04-23 13:29:17 +0100

    improve XML validation error messages (line numbers, etc)

--- lib/readers/system_reader.rb
+++ lib/readers/system_reader.rb
@@ -24,7 +24,7 @@ class SystemReader
     # validate scenario XML against schema
     begin
       xsd = Nokogiri::XML::Schema(File.open(SCENARIO_SCHEMA_FILE))
-      xsd.validate(doc).each do |error|
+      xsd.validate(scenario_file).each do |error|
         Print.err "Error in scenario configuration file (#{scenario_file}):"
         Print.err '    ' + error.message
         exit
