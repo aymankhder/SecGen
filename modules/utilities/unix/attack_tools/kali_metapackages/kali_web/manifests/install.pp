d15153dacb2a9ea9471a2baa95c16738df6acf53
thomashaw <thomashaw@gmail.com>
2021-06-09 17:09:48 +0100

    (temporary) ZAP downgrade (fix)

--- modules/utilities/unix/attack_tools/kali_metapackages/kali_web/manifests/install.pp
+++ modules/utilities/unix/attack_tools/kali_metapackages/kali_web/manifests/install.pp
@@ -8,7 +8,10 @@ class kali_web::install{
     ensure => 'installed',
   } ~>
   exec { 'safely remove the zaproxy package':
-    command => "apt-cache depends kali-tools-web | grep 'Depends:' | awk {'print \$2'} | xargs apt-mark manual && apt-get remove zaproxy"
+    command => "apt-cache depends kali-tools-web | grep 'Depends:' | awk {'print \$2'} | xargs apt-mark manual"
+  } ~>
+  package{ 'zaproxy':
+    ensure => 'absent',
   } ~>
   exec { 'create /opt/zaproxy-2.9.0':
     command => 'mkdir /opt/zaproxy-2.9.0',
