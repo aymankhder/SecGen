a97100b7fb85760505afe53f471346b22d7fe2fc
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2021-11-18 10:57:25 +0000

    IPS workaround

--- modules/utilities/unix/attack_tools/kali_metapackages/kali_top10/manifests/install.pp
+++ modules/utilities/unix/attack_tools/kali_metapackages/kali_top10/manifests/install.pp
@@ -1,5 +1,11 @@
 class kali_top10::install{
-  package { ['kali-tools-top10', 'nfs-common', 'ftp']:
-    ensure => 'installed',
-  }
+  # package { ['kali-tools-top10', 'nfs-common', 'ftp']:
+  #   ensure => 'installed',
+  # }
+  ensure_packages(['nfs-common', 'ftp'])
+
+  # The following is a workaround for the crackmapexec package triggering our IPS
+  # almost everything in kali-tools-top10:
+  ensure_packages(['aircrack-ng', 'burpsuite', 'hydra', 'john', 'metasploit-framework', 'nmap', 'responder', 'sqlmap', 'wireshark'])
+  # except crackmapexec
 }
